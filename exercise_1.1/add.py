b63ad008bb3444abd2bda15e783cfea9287e239b
n-townsend <132301374+n-townsend@users.noreply.github.com>
2023-12-05 14:02:22 -0500

    first commit

---
+++ exercise_1.1/add.py
@@ -0,0 +1,11 @@
+# Prompt user to enter the first number
+a = int(input("Enter the first number: "))
+
+# Prompt user to enter the second number
+b = int(input("Enter the second number: "))
+
+# Add the two numbers and store the result in variable c
+c = a + b
+
+# Print the value of c
+print("The sum of", a, "and", b, "is", c)
